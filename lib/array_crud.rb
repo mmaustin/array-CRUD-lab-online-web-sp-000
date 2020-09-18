0dcbd175e760bf1a8af5755eb64ac6c44c237b3a
mccray austin <tubach75@hotmail.com>
2020-09-18 21:19:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,8 +27,9 @@ end
 remove_element_from_start_of_array(["wow", "I", "am", "really", "learning", "arrays!"])
 
 def retrieve_element_from_index(array, index_number)
-
+  array[index_number]
 end
+retrieve_element_from_index(["wow", "I", "am", "really", "learning", "arrays!"], 2)
 
 def retrieve_first_element_from_array(array)
 
